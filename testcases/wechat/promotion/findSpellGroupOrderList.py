143b95ad73b7dbade988368e707ba8cf5957de26
leo <48340932+amangeyashang@users.noreply.github.com>
2021-03-11 10:31:34 +0800

    更新内容

--- testcases/wechat/promotion/findSpellGroupOrderList.py
+++ testcases/wechat/promotion/findSpellGroupOrderList.py
@@ -14,7 +14,7 @@ class TestCaseDemoTestcaseRequest(HttpRunner):
     )
     teststeps = [
         Step(
-            RunRequest("拼团订单列表-001")
+            RunRequest("拼团订单列表-全部-001")
                 .with_variables(**{})
                 .post("/spellGroup/findSpellGroupOrderList")
                 .with_headers(
@@ -25,9 +25,88 @@ class TestCaseDemoTestcaseRequest(HttpRunner):
             )
                 .with_json(
                 {
-                    "vendorCode": "VC10003",
-                    "productCode": "PD19112500000021",
-                    "searchPageVo":'null'
+                    "groupState":"ALL",
+                    "memberId":2494,
+                    "searchPageVo":
+                        {
+                            "pageIndex":1,
+                            "pageSize":10
+                        }
+                }
+            )
+                .validate()
+                .assert_equal("status_code",200)
+                .assert_equal("body.msg","success")
+        ),
+        Step(
+            RunRequest("拼团订单列表-拼团中-002")
+                .with_variables(**{})
+                .post("/spellGroup/findSpellGroupOrderList")
+                .with_headers(
+                **{
+                    "User-Agent":"HttpRunner/${get_httprunner_version()}",
+                    "Content-Type":"application/json",
+                }
+            )
+                .with_json(
+                {
+                    "groupState":"GROUP_ING",
+                    "memberId":2494,
+                    "searchPageVo":
+                        {
+                            "pageIndex":1,
+                            "pageSize":10
+                        }
+                }
+            )
+                .validate()
+                .assert_equal("status_code",200)
+                .assert_equal("body.msg","success")
+        ),
+        Step(
+            RunRequest("拼团订单列表-拼团成功-003")
+                .with_variables(**{})
+                .post("/spellGroup/findSpellGroupOrderList")
+                .with_headers(
+                **{
+                    "User-Agent":"HttpRunner/${get_httprunner_version()}",
+                    "Content-Type":"application/json",
+                }
+            )
+                .with_json(
+                {
+                    "groupState":"GROUP_SUCCESS",
+                    "memberId":2494,
+                    "searchPageVo":
+                        {
+                            "pageIndex":1,
+                            "pageSize":10
+                        }
+                }
+            )
+                .validate()
+                .assert_equal("status_code",200)
+                .assert_equal("body.msg","success")
+        ),
+        Step(
+            RunRequest("拼团订单列表-拼团失败-004")
+                .with_variables(**{})
+                .post("/spellGroup/findSpellGroupOrderList")
+                .with_headers(
+                **{
+                    "User-Agent":"HttpRunner/${get_httprunner_version()}",
+                    "Content-Type":"application/json",
+                }
+            )
+                .with_json(
+                {
+                    "groupState":"GROUP_FAIL",
+                    "memberId":2494,
+                    "searchPageVo":
+                        {
+                            "pageIndex":1,
+                            "pageSize":10
+                        }
                 }
             )
                 .validate()
